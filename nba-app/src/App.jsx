e0454a58949fb467d7d9640408633cc3e5fee9b2
Camilla Barbosa <camillabarbosa5@gmail.com>
2023-11-28 20:36:24 -0300

    tela de inicio home page nba

--- nba-app/src/App.jsx
+++ nba-app/src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-
-    </>
+   <div>
+    <p></p>
+   </div>
+  
   )
 }
 
--- nba-app/src/main.jsx
+++ nba-app/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Cards from './pages/teams/index.jsx'
+import Homepage from './component/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Cards />
+    <Homepage />
   </React.StrictMode>
 )
